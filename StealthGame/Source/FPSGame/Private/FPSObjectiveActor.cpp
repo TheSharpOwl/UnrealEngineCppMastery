7b5a82478cb889e92472c1749154b1043b9c0973
Silver <ziadxkabakibi@hotmail.com>
2019-09-01 09:45:46 +0300

    video 10 done

---
+++ StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSObjectiveActor.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AFPSObjectiveActor::AFPSObjectiveActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSObjectiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSObjectiveActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
