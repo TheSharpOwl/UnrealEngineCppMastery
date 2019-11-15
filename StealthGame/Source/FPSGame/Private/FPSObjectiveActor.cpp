6252ecf6cf004f113d53cd48c2d3d3bff89c355d
Silver <ziadxkabakibi@hotmail.com>
2019-11-15 13:44:51 +0300

    video 12 done

--- StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ StealthGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -5,12 +5,12 @@
 #include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
 
 // Sets default values
 AFPSObjectiveActor::AFPSObjectiveActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//Deleted "CanTick" from here because tick is not needed
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
@@ -32,18 +32,20 @@ void AFPSObjectiveActor::BeginPlay()
 }
 
 
-// Called every frame
-void AFPSObjectiveActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+//Deleted the Tick Function because it's' not needed
 
 void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
 {
 	Super::NotifyActorBeginOverlap(OtherActor);
 
 	PlayEffects();
+
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (MyCharacter)
+	{
+		MyCharacter->bIsCarryingObjective = true;
+		Destroy();
+	}
 }
 
 void AFPSObjectiveActor::PlayEffects()
