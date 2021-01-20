c7755cd42adbf8ad1a93c4d4dba2ce0d0ce758e9
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-01-20 23:59:04 +0300

    46 Done.

--- CoopGame/Source/CoopGame/Public/SCharacter.h
+++ CoopGame/Source/CoopGame/Public/SCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "SCharacter.generated.h"
 
+class UCameraComponent;
+
+
 UCLASS()
 class COOPGAME_API ASCharacter : public ACharacter
 {
@@ -18,6 +21,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComp;
 
 public:	
 	// Called every frame
